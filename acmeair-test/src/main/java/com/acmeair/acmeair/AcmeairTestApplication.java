133ec3569804aa4fb6a63f5b3ec95edffb6782de
ziliang guo <ziliang.guo@sohu.com>
2023-01-13 10:45:50 +0800

    修改注释

---
+++ acmeair-test/src/main/java/com/acmeair/acmeair/AcmeairTestApplication.java
@@ -0,0 +1,13 @@
+package com.acmeair.acmeair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeairTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmeairTestApplication.class, args);
+    }
+
+}
---
+++ acmeair-test/src/test/java/com/acmeair/acmeair/AcmeairTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acmeair.acmeair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeairTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
